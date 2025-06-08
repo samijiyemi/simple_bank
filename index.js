a3ae171774168b7c785ff8d9a1834937921d7ef9
ogbenisamu <samijiyemi@gmail.com>
2025-06-08 21:30:34 +0100

    Specify path for dotenv configuration in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-require("dotenv").config();
+// const path = require("path");
+require("dotenv").config({ path: "./config/.env" });
 const app = express();
 const port = process.env.PORT || 3000;
 
