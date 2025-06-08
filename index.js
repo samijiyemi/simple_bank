b96f1b57d75442f8904dbf1e345b2119ba8b8cce
ogbenisamu <samijiyemi@gmail.com>
2025-06-08 21:19:46 +0100

    Update environment variable names for consistency with .env configuration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
