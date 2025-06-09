079cc1d3f5ab3b5560d99820f18e11fb113726e8
ogbenisamu <samijiyemi@gmail.com>
2025-06-09 06:38:36 +0100

    Add logging capabilities with Winston and Morgan; initialize log files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
 import { isSpoofedBot } from "@arcjet/inspect";
 import express from "express";
 import dotenv from "dotenv";
+import { createContextLogger } from "./config/logger.js"; // Ensure logger is initialized
 
 import accountRoutes from "./routes/accountRoutes.js";
 
